8365075d37d12f11ac35f7047db7d473c4061a5a
babicvlcnt <bcfrota@id.uff.br>
2022-02-23 17:42:21 -0300

    first commit - done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+class Contador extends Component {
+  state = {
+    number: 0
+  };
+  add = () => {
+    if (this.state.number < 10) {
+      this.setState({
+        number: this.state.number + 1
+      });
+    }
+  }
+  subt = () => {
+    if (this.state.number > 0) {
+      this.setState ({
+        number: this.state.number - 1
+      });
+    }
+  }
+    clear = () => {
+      if (this.state.number > 0){
+        this.setState ({
+          number: 0
+        });
+      }
+    }
+  render() {
+    return (
+      <div className="contain-box">
+        <h1>Contador</h1>
+        <div className="inbox">
+          <h2>{this.state.number}</h2>
+          <button onClick = {this.add}>+1</button>
+          <button onClick = {this.clear}>C</button>
+          <button onClick = {this.subt}>-1</button>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default Contador;
